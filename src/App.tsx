f4b214aa67f2296e4b4d557d167b8129a10fa6b9
D <dracokd19@gmail.com>
2024-02-04 18:19:27 +0545

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='App'>
+      <div className='container'>
+      <h2>ToDo List</h2>
+      <input type="text" name="" id="" className='input'/>
+      <button>Add</button>
+      </div>
+      <div className='show'>
+        <p>Title and shit</p>
+        <span>laude</span>
+        </div>
+      </div>
+      
+    </>
   );
 }
 
