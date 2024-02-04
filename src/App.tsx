7a9332f26d39d158ad7d026350942e0481c4fe25
D <dracokd19@gmail.com>
2024-02-04 19:26:26 +0545

    particed usestate and some array function

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
 
 function App() {
+  const[todo,setTodo] = useState<string>('');
+  const[note,setNote] = useState<string[]>([]);
+  const handleTodo = (e: ChangeEvent<HTMLInputElement>) =>{
+    setTodo(e.target.value);
+  }
+  const addTodo = () =>{
+    if(todo.trim() !== ''){
+      setNote([...note, todo]);
+      setTodo('');
+    }
+  }
+
+  const removeTodo = (index:number) =>{
+    const updateNote = [...note];
+    updateNote.splice(index,1);
+    setNote(updateNote);
+  }
+  const clearAll = () =>{
+    setNote([]);
+  }
+
   return (
     <>
     <div className='App'>
       <div className='container'>
       <h2>ToDo List</h2>
-      <input type="text" name="" id="" className='input'/>
-      <button>Add</button>
+      <input type="text" name="" id="" className='input' value={todo} onChange={handleTodo}/>
+      <button onClick={addTodo}>Add</button>
+      <button onClick={clearAll}>Clear All</button>
       </div>
-      <div className='show'>
-        <p>Title and shit</p>
-        <span>laude</span>
+      
+        {note.map((item, index)=>(
+          <div className='show'>
+          <p>{item}</p>
+          <div className='click' onClick={() => removeTodo(index)}><button>Delete</button></div>
         </div>
+        ))}
+       
       </div>
       
     </>
